5b335997b4e1735bae10f3aa1233b8d1df38c2ee
osha7 <ogroetz@gmail.com>
2020-12-26 09:07:38 -0700

    home headers

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,7 +4,14 @@ import '../../css/home.css';
 export function Home(props) {
 
     return (
-        <div className="home-image">
-        </div>
+        <>
+            <div className="home-image">
+            </div>
+            <div className="home-content" >
+                <h1 className="home-header">Telling The Full American Story</h1>  
+                <h3>Justo quando sit ne, menandri repudiare ea est, at minimum constituam cotidieque pri. An tempor aperiri nostrum eam.</h3>
+                <h3 className="second-h3"> Zril accommodare quo no, pri equidem suavitate persequeris ut, eu adolescens omittantur consectetuer mea.</h3>
+            </div>
+        </>
     )
 }
