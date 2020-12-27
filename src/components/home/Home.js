47037a61d1435fa486ed5391fcf28b136daa260d
osha7 <ogroetz@gmail.com>
2020-12-27 11:38:47 -0700

    three stories mapping

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,35 +1,42 @@
-import React, {useState, useEffect} from 'react'
-import '../../css/home.css';
-import ThreeStories from './ThreeStories'
+import React, { useState, useEffect } from "react";
+import "../../css/home.css";
+import ThreeStories from "./ThreeStories";
 
 export function Home(props) {
-    const [data, setData] = useState({stories: []})
+  const [data, setData] = useState({ stories: [] });
 
-    const API_URL = "http://localhost:3000/"
+  const API_URL = "http://localhost:3000/";
 
-    useEffect(async () => {
-        // console.log("here")
-        const response = await fetch(API_URL + "stories")
-        const fetchData = await response.json()
-        setData(fetchData)
-        // console.log(data)
-    }, [])
-    
+  useEffect(async () => {
+    // console.log("here")
+    const response = await fetch(API_URL + "stories");
+    const fetchData = await response.json();
+    setData(fetchData);
+    // console.log(data)
+  }, []);
 
-    return (
-        <>
-        {console.log("here", data)}
-            <div className="home-image">
-            </div>
-            <div className="home-headers" >
-                <h1 className="first-h3">Telling The Full American Story</h1>  
-                <h3>Justo quando sit ne, menandri repudiare ea est, at minimum constituam cotidieque pri. An tempor aperiri nostrum eam.</h3>
-                <h3 className="second-h3"> Zril accommodare quo no, pri equidem suavitate persequeris ut, eu adolescens omittantur consectetuer mea.</h3>
-            </div>
-            <div className="first-three-story-cards">
-                {console.log("mapping", data.stories)}
-                <ThreeStories  stories={data.stories} />
-            </div>
-        </>
-    )
+  return (
+    <>
+      {/* {console.log("here", data)} */}
+      <div className="home-image"></div>
+      <div className="home-headers">
+        <h1 className="first-h3">Telling The Full American Story</h1>
+        <h3>
+          Justo quando sit ne, menandri repudiare ea est, at minimum constituam
+          cotidieque pri. An tempor aperiri nostrum eam.
+        </h3>
+        <h3 className="second-h3">
+          {" "}
+          Zril accommodare quo no, pri equidem suavitate persequeris ut, eu
+          adolescens omittantur consectetuer mea.
+        </h3>
+      </div>
+      {data.stories.length > 0 && (
+        <div className="first-three-story-cards">
+          {/* {console.log("mapping", data.stories.length === 0)} */}
+          <ThreeStories stories={data.stories} />
+        </div>
+      )}
+    </>
+  );
 }
--- src/components/home/ThreeStories.js
+++ src/components/home/ThreeStories.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function ThreeStories(props) {
+  const latestStory = props.stories[props.stories.length - 1];
+  const secondLatestStory = props.stories[props.stories.length - 2];
+  const thirdLatestStory = props.stories[props.stories.length - 3];
 
-    
-    return (
-        <div>
-            HERE
-            {console.log("these props", props.stories[props.stories.length - 1])}
-        </div>
-    );
+  const [latestStories] = useState([
+    latestStory,
+    secondLatestStory,
+    thirdLatestStory,
+  ]);
+
+  return (
+    <>
+      {latestStories.map((story) => {
+        <div></div>;
+      })}
+      {/* {console.log("threeStories return:", latestStories)} */}
+      HERE
+    </>
+  );
 }
 
 export default ThreeStories;
