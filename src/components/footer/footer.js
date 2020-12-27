43d4946741b58e1d98d8d416f0a467a49d165a6b
osha7 <ogroetz@gmail.com>
2020-12-27 14:51:59 -0700

    add logo to navbar

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,14 @@ import '../../css/footer.css';
 export const Footer = () => {
     return(
         <div className="footer">
-        HELLO
+        <div class="row">
+                <div class="col-3-of-4">
+                    Col 3 of 4
+                </div>
+                <div class="col-1-of-4">
+                    <h2>Become a Contributing Writer</h2>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../../css/navbar.css';
 import { NavLink } from "react-router-dom";
+import SOSDLogo from '../../css/SOSD.svg'
 
 const link = {
   width: "100px",
@@ -15,6 +16,7 @@ export const NavBar = () => {
   return (
     <>
       <div className="navbar-header">
+        <img src={SOSDLogo} alt="SOSD logo" width="60" height="70" />
         <h1>Stories of San Diego</h1>
       </div>
       <div className="navbar-links">
