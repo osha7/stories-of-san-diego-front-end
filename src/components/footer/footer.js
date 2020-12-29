44e1704065e485884e55e941ec01e966d5e3c300
osha7 <ogroetz@gmail.com>
2020-12-29 16:48:59 -0700

    formatting fixes document wide

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,50 +1,49 @@
 import "../../css/footer.css";
 
 export const Footer = () => {
-  return (
-      <main className="footer-sections">
-    <section className="footer">
-      {/* <div className="row"> */}
-        {/* <div className="col-3-of-4"> */}
-          <ul className="nav">
-            <li className="nav__item">
-              <a href="/" alt="Home" className="nav__link">
-                HOME
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="/stories" alt="Stories" className="nav__link">
-                STORIES
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="/about" alt="About" className="nav__link">
-                ABOUT
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="/contact-us" alt="Contact Us" className="nav__link">
-                CONTACT
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="/admin" alt="Admin" className="nav__link">
-                ADMIN
-              </a>
-            </li>
-          </ul>
-          <p className="copyright">
-              © Copyright 2020 Stories of San Diego
-          </p>
-        {/* </div> */}
-       
-       {/* </div> */}
-    </section>
-     <aside className="contributing-writer">
-     <a href="/contact-us" alt="Read More">
-       <h2>Become a Contributing Writer</h2>
-     </a>
-   </aside>
-   </main>
-  );
+    return (
+        <main className="footer-sections">
+            <section className="footer">
+                <ul className="nav">
+                    <li className="nav__item">
+                        <a href="/" alt="Home" className="nav__link">
+                            HOME
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="/stories" alt="Stories" className="nav__link">
+                            STORIES
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="/about" alt="About" className="nav__link">
+                            ABOUT
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a
+                            href="/contact-us"
+                            alt="Contact Us"
+                            className="nav__link"
+                        >
+                            CONTACT
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="/admin" alt="Admin" className="nav__link">
+                            ADMIN
+                        </a>
+                    </li>
+                </ul>
+                <p className="copyright">
+                    © Copyright 2020 Stories of San Diego
+                </p>
+            </section>
+            <aside className="contributing-writer">
+                <a href="/contact-us" alt="Read More">
+                    <h2>Become a Contributing Writer</h2>
+                </a>
+            </aside>
+        </main>
+    );
 };
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,48 +1,50 @@
 import React, { useState, useEffect } from "react";
 import "../../css/home.css";
 import ThreeStories from "./ThreeStories";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export function Home(props) {
-  const [data, setData] = useState({ stories: [] });
+    const [data, setData] = useState({ stories: [] });
+    
+    const API_URL = "http://localhost:3000/";
 
-  const API_URL = "http://localhost:3000/";
+    useEffect(() => {
+        // console.log("here")
+        const fetchStories = async () => {
+            const response = await fetch(API_URL + "stories");
+            const fetchData = await response.json();
+            setData(fetchData);
+        };
+        // console.log(data)
+        fetchStories();
+    }, []);
 
-  useEffect(() => {
-    // console.log("here")
-    const fetchStories = async() => {
-        const response = await fetch(API_URL + "stories");
-        const fetchData = await response.json();
-        setData(fetchData);
-    }
-    // console.log(data)
-    fetchStories()
-  }, []);
-
-  return (
-    <>
-      {/* {console.log("here", data)} */}
-      <div className="home-image"></div>
-      <div className="home-headers">
-        <h1>Telling The Full American Story</h1>
-        <h3>
-          Justo quando sit ne, menandri repudiare ea est, at minimum constituam
-          cotidieque pri. An tempor aperiri nostrum eam.
-        </h3>
-        <h3 className="second-h3">
-          Zril accommodare quo no, pri equidem suavitate persequeris ut, eu
-          adolescens omittantur consectetuer mea.
-        </h3>
-      </div>
-      {data.stories.length > 0 && (
-        <div className="first-three-story-cards">
-          {/* {console.log("mapping", data.stories.length === 0)} */}
-          <ThreeStories stories={data.stories} />
-        </div>
-      )}
-      <div className="home-headers-2">
-        <Link to="/contact-us" ><h3>Share YOUR Story with us</h3></Link>
-      </div>
-    </>
-  );
+    return (
+        <>
+            {/* {console.log("here", data)} */}
+            <div className="home-image"></div>
+            <div className="home-headers">
+                <h1>Telling The Full American Story</h1>
+                <h3>
+                    Justo quando sit ne, menandri repudiare ea est, at minimum
+                    constituam cotidieque pri. An tempor aperiri nostrum eam.
+                </h3>
+                <h3 className="second-h3">
+                    Zril accommodare quo no, pri equidem suavitate persequeris
+                    ut, eu adolescens omittantur consectetuer mea.
+                </h3>
+            </div>
+            {data.stories.length > 0 && (
+                <div className="first-three-story-cards">
+                    {/* {console.log("mapping", data.stories.length === 0)} */}
+                    <ThreeStories stories={data.stories} />
+                </div>
+            )}
+            <div className="home-headers-2">
+                <Link to="/contact-us">
+                    <h3>Share YOUR Story with us</h3>
+                </Link>
+            </div>
+        </>
+    );
 }
