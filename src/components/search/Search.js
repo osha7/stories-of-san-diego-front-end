2812252de932a1e02d19fe06d0bcb7e7027a73ae
osha7 <ogroetz@gmail.com>
2021-01-06 16:08:19 -0700

    login form working correctly with input proper padding

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,41 +1,69 @@
 import React, { useState } from "react";
+import { useFetchStoriesData } from '../storiespage/useFetchStoriesData';
 
-function Search(props) {
-
-    const API_URL = "http://localhost:3000"
+function Search() {
     
+    const [data] = useFetchStoriesData()
     const [searchTerm, setSearchTerm] = useState("");
+    const [searchTerms, setSearchTerms] = useState([])
     const [searchResults, setSearchResults] = useState([])
     
     // const query = searchTerm.split(" ").join('').toLowerCase()
-
+    // console.log("search data", data.stories)
+    
+    const API_URL = "http://localhost:3000"
     const searchURL = "/search.json?q=" + searchTerm
 
     const handleOnChange = (e) => {
         // debugger
-        setSearchTerm(e.target.value)
+        let values = e.target.value
+        setSearchTerm(values)
+        let valuesSplit = values.split(" ")
+        console.log("valuesSplit", valuesSplit) // ["fun", "and"]
+        setSearchTerms(valuesSplit)
     };
 
     const handleOnSubmit = (e) => {
         e.preventDefault();
-        const fetchSearchResults = async () => {
-            const response = await fetch(API_URL + searchURL)
-            const fetchSearchData = await response.json()
-            setSearchResults(fetchSearchData.stories)
+        // const fetchSearchResults = async () => {
+        //     const response = await fetch(API_URL + searchURL)
+        //     const fetchSearchData = await response.json()
+        //     setSearchResults(fetchSearchData.stories)
+        // }
+        // fetchSearchResults()
+        const filteredStories = []
+        if (searchTerms.length > 0) {
+            for (let i = 0; i < searchTerms.length; i++) {
+                filteredStories.push(...data.stories.filter(story => story.contributor.toLowerCase().includes(searchTerms[i].toLowerCase()) ||
+                story.story.toLowerCase().includes(searchTerms[i].toLowerCase()) ||
+                story.summary.toLowerCase().includes(searchTerms[i].toLowerCase())
+                ))
+            }
+            setSearchResults(filteredStories)
         }
-        fetchSearchResults()
         
     };
-    console.log("results", searchResults)
+    // console.log("results", searchResults)
 
     // const retrievingQuery = (searchTerm) => {
     //     // debugger
     //     // let newQueryString = searchTerm.split;
     // };
+
+    const stories = searchResults.map(story => {
+        return (
+            <li style={{ listStyleType: "none" }} key={story.id}>
+                <p><strong>Story Submitted By: </strong>{story.contributor}</p>
+                <p><strong>Story Summary: </strong>{story.summary}</p>
+                <div aria-hidden="true">__________</div>
+            </li>
+        )
+    })
     return (
         <div className="search-articles">
             <div className="search-box">
                 <form onSubmit={handleOnSubmit}>
+                    <label htmlFor="searchTerm">Search:</label>
                     <input
                         className="search-input"
                         name="searchTerm"
@@ -48,6 +76,11 @@ function Search(props) {
                         <i className="fa fa-search" aria-hidden="true"></i>
                     </a>
                 </form>
+            </div>
+            <div className="search-container">
+                <ul>
+                    {stories}
+                </ul>
             </div>
         </div>
     );
