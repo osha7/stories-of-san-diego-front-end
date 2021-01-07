c1273909f61236b808594cad791608035083a4ec
osha7 <ogroetz@gmail.com>
2021-01-07 02:35:03 -0700

    search bars working on search page and on stories page

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useFetchStoriesData } from '../storiespage/useFetchStoriesData';
 import '../../css/search.css'
+import { Link } from 'react-router-dom';
+
 
 function Search() {
     
@@ -34,54 +36,76 @@ function Search() {
 
     const stories = searchResults.map(story => {
         return (
-            <li style={{ listStyleType: "none" }} key={story.id}>
-                <p><strong>Story Submitted By: </strong>{story.contributor}</p>
+            <li style={{ listStyleType: "none" }} key={story.id} >
+                {/* <p><strong>Story Submitted By: </strong>{story.contributor}</p>
                 <p><strong>Story Summary: </strong>{story.summary}</p>
-                <div aria-hidden="true">__________</div>
+                <div aria-hidden="true">__________</div> */}
+                <div className="recent-stories" key={story.id}>
+                    <div className="button-container">
+                        <img
+                            className="story-image"
+                            src={story.image}
+                            alt={story.contributor}
+                        />
+                        {/* {console.log("stories", story)} */}
+                        <Link to={'/stories/' + story.id} story_id={story.id} >Read More</Link>
+                        {/* <a href="#" alt="Read More">
+                            Read More
+                        </a> */}
+                    </div>
+                    <div className="story-summary">
+                        <h4>{story.summary}</h4>
+                    </div>
+                </div>
             </li>
         )
     })
-    return (
-        <div className="search-articles">
-            {/* <div className="search-box">
-                <form onSubmit={handleOnSubmit}>
-                    <label htmlFor="searchTerm">Search:</label>
-                    <input
-                        className="search-input"
-                        name="searchTerm"
-                        type="search"
-                        value={searchTerm}
-                        onChange={handleOnChange}
-                        placeholder="Search For Stories..."
-                    />
-                    <a className="search-btn" onClick={handleOnSubmit}>
-                        <i className="fa fa-search" aria-hidden="true"></i>
-                    </a>
-                </form>
-            </div> */}
-            <div class="wrapper">
-                <div class="container">
-                    <div class="search_wrap search_wrap_2">
-                        <div class="search_box">
-                            <div class="btn btn_common">
-                                <i class="fa fa-search"></i>
+
+    if (stories.length === 0) {
+        return (
+            <div className="search-articles">
+                <div class="wrapper" style={{"height" : "400px"}}>
+                    <div class="container">
+                        <div class="search_wrap search_wrap_2">
+                            <div class="search_box">
+                                <div class="btn btn_common">
+                                    <i class="fa fa-search"></i>
+                                </div>
+                                <form onSubmit={handleOnSubmit}>
+                                <input type="search" class="input" value={searchTerm} placeholder="Search For Stories..." onChange={handleOnChange}/>
+                                </form>
                             </div>
-                            <form onSubmit={handleOnSubmit}>
-                            <input type="search" class="input" value={searchTerm} placeholder="Search For Stories..." onChange={handleOnChange}/>
-                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-
-
-            <div className="search-container">
-                <ul>
-                    {stories}
-                </ul>
+        )
+    } else {
+        return (
+            <div className="search-articles">
+                <div class="wrapper">
+                    <div class="container">
+                        <div class="search_wrap search_wrap_2">
+                            <div class="search_box">
+                                <div class="btn btn_common">
+                                    <i class="fa fa-search"></i>
+                                </div>
+                                <form onSubmit={handleOnSubmit}>
+                                <input type="search" class="input" value={searchTerm} placeholder="Search For Stories..." onChange={handleOnChange}/>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div className="search-container">
+                        <ul className="search-story-cards">
+                            {stories}
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Search;
--- src/components/storiespage/Stories.js
+++ src/components/storiespage/Stories.js
@@ -4,31 +4,27 @@ import { useFetchStoriesData } from "../storiespage/useFetchStoriesData";
 import StoriesDiv from "./StoriesDiv";
 import Pagination from '../Pagination'
 
-// export function Stories(props) {
-
-//     const [data] = useFetchStoriesData();
-
-//     const [offset, setOffset] = useState(0);
-//     const [perPage] = useState(10);
-//     const [pageCount, setPageCount] = useState(0);
-
-//     return <div></div>;
-
-//     const slice = data.slice(offset, offset + perPage)
-// }
-
 export function Stories(props) {
     const [data] = useFetchStoriesData();
     const [currentPage, setCurrentPage] = useState(1)
     const [storiesPerPage]  = useState(12)
-    // console.log("stories", data);
+
     const stories = data.stories
 
-    // console.log(stories.length)
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchTerms, setSearchTerms] = useState([])
+    const [searchResults, setSearchResults] = useState([])
 
     const indexOfLastStory = currentPage * storiesPerPage
     const indexOfFirstStory = indexOfLastStory - storiesPerPage
-    const currentStories = stories.slice(indexOfFirstStory, indexOfLastStory)
+    let currentStories
+
+    if (searchResults.length === 0) {
+        currentStories = stories.slice(indexOfFirstStory, indexOfLastStory)
+    } else {
+        currentStories = searchResults.slice(indexOfFirstStory, indexOfLastStory)
+    }
+
     const howManyPages = Math.ceil(stories.length/storiesPerPage)
     // console.log("indexOfLastStory", indexOfLastStory)
     // console.log("indexOfFirstStory", indexOfFirstStory)
@@ -36,10 +32,42 @@ export function Stories(props) {
     // console.log("howManyPages", howManyPages)
     // console.log("story props", props)
 
+    const storiesFilterOnChange = (e) => {
+        e.preventDefault();
+        let values = e.target.value
+        setSearchTerm(values)
+        let valuesSplit = values.split(" ")
+        console.log("valuesSplit", valuesSplit)
+        setSearchTerms(valuesSplit)
+
+        const filteredStories = []
+        if (searchTerms.length > 0) {
+            for (let i = 0; i < searchTerms.length; i++) {
+                filteredStories.push(...data.stories.filter(story => story.contributor.toLowerCase().includes(searchTerms[i].toLowerCase()) ||
+                story.story.toLowerCase().includes(searchTerms[i].toLowerCase()) ||
+                story.summary.toLowerCase().includes(searchTerms[i].toLowerCase())
+                ))
+            }
+            setSearchResults(filteredStories)
+        }
+        
+    };
+
     return (
         <div className="all-stories">
-            <div className="stories-headers">
-                <h1>All Contributed Stories</h1>
+            <div className="search-articles">
+                <div class="wrapper">
+                    <div class="container">
+                        <div class="search_wrap search_wrap_2">
+                            <div class="search_box">
+                                <div class="btn btn_common">
+                                    <i class="fa fa-search"></i>
+                                </div>
+                                <input type="search" class="input" value={searchTerm} placeholder="Search For Stories..." onChange={storiesFilterOnChange}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="first-three-story-cards">
             <StoriesDiv stories={currentStories} />
