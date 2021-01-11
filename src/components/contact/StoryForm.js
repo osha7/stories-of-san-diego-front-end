67c8c0ed58f3b22b8a8cb79b677fecfcca126228
osha7 <ogroetz@gmail.com>
2021-01-11 14:15:17 -0700

    image submission allowed on contact us/submit story

--- src/components/contact/StoryForm.js
+++ src/components/contact/StoryForm.js
@@ -42,11 +42,12 @@ function StoryForm() {
                 title: 'Email Successfully Sent',
                 icon: 'success'
             })
+            e.target.reset()
         })
         // Email Failed to send Error alert
         .catch(err => {
             Swal.fire({
-                title: 'Email Failed to Send',
+                title: `Email Failed to Send, ${err.text}`,
                 icon: 'error'
             })
             console.error('Email Error:', err)
@@ -58,7 +59,7 @@ function StoryForm() {
         // setSubmissionName('')
         // setStoryChange('')
 
-        e.target.reset()
+        
 
     }
 
@@ -67,33 +68,35 @@ function StoryForm() {
             <h2>Submit Your Story / Envía Tu Historia</h2>
             <form onSubmit={onSubmit} >
                 <div className="story-form-field">
-                    <label for="fromName" >NAME / NOMBRE: </label>
+                    <label htmlFor="fromName" >NAME / NOMBRE: </label>
                     <input id="fromName" type="text" name="fromName" className="" />
                 </div>
 
                 <div className="story-form-field">
-                    <label for="replyTo" >EMAIL / CORREO DE ELECTRONICA: </label>
-                    <input id="replyTo" type="email" name="replyTo" className="" />
-                </div>
-
-                <div className="story-form-field">
-                    <label for="phoneNumber">PHONE / TELEPHONO: </label>
-                    <input id="phoneNumber" type="text" name="phoneNumber" className="" />
-                </div>
-
-                <div className="story-form-field">
-                    <label for="image">LINK TO IMAGE. (WE  CAN'T ACCEPT FILES AT THIS TIME) / ENLACE A LA IMAGEN. (NO PODEMOS ACEPTAR ARCHIVOS EN ESTE MOMENTO): </label>
-                    <input id="image" type="text" name="image" className="" /> <br />
-                    <span>Good sites to host images: <a href="https://cloudinary.com/">Cloudinary</a> or <a href="https://imgur.com/">Imgur</a></span>
-                </div>
-
-                <div className="story-form-field">
-                    <label for="submissionName" >WHAT NAME WOULD YOU LIKE US TO USE ON YOUR SUBMISSION? / CUAL ES EL NOMBRE USAR EN EL SUBMISSION? </label>
+                    <label htmlFor="submissionName" >WHAT NAME WOULD YOU LIKE US TO USE ON YOUR SUBMISSION? / CUAL ES EL NOMBRE USAR EN EL SUBMISSION? </label>
                     <input id="submissionName" type="text" name="submissionName" className="" />
                 </div>
 
                 <div className="story-form-field">
-                    <label for="story" >STORY / HISTORIA:</label><br />
+                    <label htmlFor="replyTo" >EMAIL / CORREO DE ELECTRONICA: </label>
+                    <input id="replyTo" type="email" name="replyTo" className="" />
+                </div>
+
+                <div className="story-form-field">
+                    <label htmlFor="phoneNumber">PHONE / TELEPHONO: </label>
+                    <input id="phoneNumber" type="text" name="phoneNumber" className="" />
+                </div>
+
+                <div className="story-form-field">
+                    {/* <label for="image">LINK TO IMAGE. (WE  CAN'T ACCEPT FILES AT THIS TIME) / ENLACE A LA IMAGEN. (NO PODEMOS ACEPTAR ARCHIVOS EN ESTE MOMENTO): </label> */}
+                    {/* <input id="image" type="text" name="image" className="" /> <br />
+                    <span>Good sites to host images: <a href="https://cloudinary.com/">Cloudinary</a> or <a href="https://imgur.com/">Imgur</a></span> */}
+                    <label>IMAGE / FOTO: </label>
+                    <input type="file" name="imageFile" id="imageFile" />
+                </div>
+
+                <div className="story-form-field">
+                    <label htmlFor="story" >STORY / HISTORIA:</label><br />
                     <textarea id="story" type="textarea" cols="60" rows="20" name="story" className="" ></textarea>
                 </div>
 
