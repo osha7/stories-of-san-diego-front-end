8f8bcb11ba87f73dc1f9f76d5edc15b6b80a72fb
osha7 <ogroetz@gmail.com>
2021-01-12 12:07:22 -0700

    change wording in contact

--- src/components/contact/MailChimpSignup.js
+++ src/components/contact/MailChimpSignup.js
@@ -6,9 +6,9 @@ function MailChimpSignup() {
     return (
         <div className="story-submission-div">
             <div className="story-submission">
-                <h4 className="header1">SIGNUP TO CONTACT LIST</h4>
+                <h4 className="header1">SIGNUP TO OUR CONTACT LIST</h4>
                 <span>
-                    Signup to receive periodic info on Stories about San Diego.
+                    Signup to receive periodic info on Stories of San Diego.
                     We promise, we won't flood your inbox!
                 </span>
             </div>
